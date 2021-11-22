65a0b9f8d37d3a47a3b7b6b0b6b5f8092cd59e73
Yeasin Arafat <yeasinarafat10@gmail.com>
2021-11-22 23:01:35 +0600

    Add custemer related models

---
+++ Ipms.Core/Models/IAuditable.cs
@@ -0,0 +1,10 @@
+﻿namespace Ipms.Core.Models
+{
+    public interface IAuditable
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid UpdatedBy { get; set; }
+    }
+}
